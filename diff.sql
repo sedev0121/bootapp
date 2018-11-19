ad0b1376743ef56a0807deb19c8eee8ce6bfc8b1
richolsu <richolsu@gmail.com>
2018-11-20 01:52:25 +0700

    入库单价钱小数误差

--- diff.sql
+++ diff.sql
@@ -3,3 +3,10 @@ ALTER TABLE venpriceadjust_main ADD attach_file_name varchar(255);
 ALTER TABLE purchase_order_main DROP COLUMN money;
 ALTER TABLE purchase_order_main DROP COLUMN sum;
 ALTER TABLE notice ADD url varchar(255);
+
+ALTER TABLE purchase_in_detail MODIFY tax_price DOUBLE;
+ALTER TABLE purchase_in_detail MODIFY nat_price DOUBLE;
+ALTER TABLE purchase_in_detail MODIFY nat_tax_price DOUBLE;
+ALTER TABLE purchase_in_detail MODIFY price DOUBLE;
+ALTER TABLE purchase_in_detail MODIFY material_price DOUBLE;
+ALTER TABLE purchase_in_detail MODIFY material_tax_price DOUBLE;
