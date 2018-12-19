5b871cbae003d9c081442a50da7f301018420f6d
admin <richolsu@gmail.com>
2018-12-20 05:49:44 +0700

    set provide classes to Unit

--- src/main/java/com/srm/platform/vendor/controller/UnitController.java
+++ src/main/java/com/srm/platform/vendor/controller/UnitController.java
@@ -1,14 +1,19 @@
 package com.srm.platform.vendor.controller;
 
+import java.io.File;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
@@ -17,20 +22,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.util.StringUtils;
 
 import com.srm.platform.vendor.model.Account;
+import com.srm.platform.vendor.model.Notice;
+import com.srm.platform.vendor.model.NoticeRead;
+import com.srm.platform.vendor.model.PermissionGroupUser;
 import com.srm.platform.vendor.model.ProvideClass;
 import com.srm.platform.vendor.model.Unit;
+import com.srm.platform.vendor.model.UnitProvide;
 import com.srm.platform.vendor.model.Vendor;
 import com.srm.platform.vendor.repository.AccountRepository;
+import com.srm.platform.vendor.repository.UnitProvideRepository;
 import com.srm.platform.vendor.repository.UnitRepository;
 import com.srm.platform.vendor.repository.VendorRepository;
+import com.srm.platform.vendor.utility.AccountSaveForm;
+import com.srm.platform.vendor.utility.Constants;
 import com.srm.platform.vendor.utility.GenericJsonResponse;
 import com.srm.platform.vendor.utility.SearchItem;
 import com.srm.platform.vendor.utility.UnitNode;
+import com.srm.platform.vendor.utility.UnitSaveForm;
+import com.srm.platform.vendor.utility.UploadFileHelper;
+import com.srm.platform.vendor.utility.Utils;
 
 @Controller
 @RequestMapping(path = "/unit")
@@ -40,6 +58,9 @@ public class UnitController extends CommonController {
 
 	@Autowired
 	private UnitRepository unitRepository;
+
+	@Autowired
+	private UnitProvideRepository unitProvideRepository;
 
 	@Autowired
 	private VendorRepository vendorRepository;
@@ -104,11 +125,13 @@ public class UnitController extends CommonController {
 			return response;
 		}
 
+		unitProvideRepository.deleteByUnitId(unit.getId());
 		unitRepository.delete(unit);
 		if (childrenUnitIds != null) {
 			unitRepository.deleteByChildIds(childrenUnitIds.split(","));
 		}
 
+		
 		return new GenericJsonResponse<>(GenericJsonResponse.SUCCESS, null, null);
 	}
 
@@ -166,7 +189,7 @@ public class UnitController extends CommonController {
 		return node;
 
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/get/{id}", produces = "application/json")
 	public Map<String, Object> get_ajax(@PathVariable("id") Long id) {
@@ -177,6 +200,29 @@ public class UnitController extends CommonController {
 		response.put("provide_classes", provideClassList);
 		return response;
 
+	}
+
+	// 用户修改
+	@Transactional
+	@PostMapping("/update")
+	public @ResponseBody GenericJsonResponse<Unit> update_ajax(UnitSaveForm unitSaveForm) {
+		Unit unit = unitRepository.findOneById(unitSaveForm.getId());
+		unit.setName(unitSaveForm.getName());
+		unitRepository.save(unit);
+
+		unitProvideRepository.deleteByUnitId(unit.getId());
+
+		List<Long> provideClassIdList = unitSaveForm.getProvideclasses();
+		if (provideClassIdList != null) {
+			for (Long id : provideClassIdList) {
+				UnitProvide temp = new UnitProvide(id, unit.getId());
+				unitProvideRepository.save(temp);
+			}
+		}
+
+		GenericJsonResponse<Unit> response = new GenericJsonResponse<>(GenericJsonResponse.SUCCESS, null, unit);
+
+		return response;
 	}
 
 }
---
+++ src/main/java/com/srm/platform/vendor/repository/UnitProvideRepository.java
@@ -0,0 +1,28 @@
+package com.srm.platform.vendor.repository;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import com.srm.platform.vendor.model.ProvideClass;
+import com.srm.platform.vendor.model.Unit;
+import com.srm.platform.vendor.model.UnitProvide;
+import com.srm.platform.vendor.utility.PermissionItem;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UnitProvideRepository extends JpaRepository<UnitProvide, Long> {
+
+
+	@Transactional
+	@Modifying(clearAutomatically = true)
+	@Query(value = "delete from unit_provide where unit_id = ?1", nativeQuery = true)
+	void deleteByUnitId(Long unitId);
+	
+
+}
