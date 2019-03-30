9a30c960a27fce5a4fab244662f929f7378fb4ec
admin <richolsu@gmail.com>
2019-03-31 06:30:04 +0700

    add 存货分类, 供应商档案分类

--- src/main/java/com/srm/platform/vendor/repository/InventoryClassRepository.java
+++ src/main/java/com/srm/platform/vendor/repository/InventoryClassRepository.java
@@ -1,9 +1,12 @@
 package com.srm.platform.vendor.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.srm.platform.vendor.model.InventoryClass;
-import com.srm.platform.vendor.model.ProvideClass;
+import com.srm.platform.vendor.searchitem.VendorSearchItem;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
@@ -11,4 +14,7 @@ import com.srm.platform.vendor.model.ProvideClass;
 public interface InventoryClassRepository extends JpaRepository<InventoryClass, Long> {
 
 	InventoryClass findOneByCode(String code);
+	
+	@Query(value = "SELECT * FROM inventory_class where (code LIKE %?1% or name LIKE %?1%)", nativeQuery = true)
+	Page<VendorSearchItem> findBySearchTerm(String search, Pageable pageable);
 }
--- src/main/java/com/srm/platform/vendor/repository/VendorClassRepository.java
+++ src/main/java/com/srm/platform/vendor/repository/VendorClassRepository.java
@@ -1,10 +1,12 @@
 package com.srm.platform.vendor.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import com.srm.platform.vendor.model.InventoryClass;
-import com.srm.platform.vendor.model.ProvideClass;
 import com.srm.platform.vendor.model.VendorClass;
+import com.srm.platform.vendor.searchitem.VendorSearchItem;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
@@ -12,4 +14,7 @@ import com.srm.platform.vendor.model.VendorClass;
 public interface VendorClassRepository extends JpaRepository<VendorClass, Long> {
 
 	VendorClass findOneByCode(String code);
+	
+	@Query(value = "SELECT * FROM vendor_class where (code LIKE %?1% or name LIKE %?1%)", nativeQuery = true)
+	Page<VendorSearchItem> findBySearchTerm(String search, Pageable pageable);
 }
